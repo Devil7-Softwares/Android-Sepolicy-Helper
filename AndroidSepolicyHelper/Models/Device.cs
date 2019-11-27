c8493914241ecbd6df077956f6d483190da42a48
Devil7DK <dineshthangavel47@gmail.com>
2019-11-27 11:17:07 +0530

    Add Device Model for Storing ADB Device Details

---
+++ AndroidSepolicyHelper/Models/Device.cs
@@ -0,0 +1,24 @@
+﻿namespace AndroidSepolicyHelper.Models
+{
+    public class Device
+    {
+        public Device(string DeviceName, string Status, string Type)
+        {
+            this.DeviceName = DeviceName.Trim();
+            this.Status = Status.Trim();
+            this.Type = Type.Trim();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", this.DeviceName, this.Type);
+        }
+
+        public string DeviceName { get; set; }
+
+        public string Status { get; set; }
+
+        public string Type { get; set; }
+    }
+}
+
