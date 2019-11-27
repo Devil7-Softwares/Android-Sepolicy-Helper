1e004c16e10dc11856972400e2187d5f8e7bfffb
Devil7DK <dineshthangavel47@gmail.com>
2019-11-27 11:18:22 +0530

    Add Helper Class for Operating with ADB

---
+++ AndroidSepolicyHelper/Utils/ADB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace AndroidSepolicyHelper.Utils
+{
+    public class ADB
+    {
+        public static string RunCommand(string Command)
+        {
+            Process process = new Process();
+            string folderName = "";
+            string fileName = "";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                folderName = "windows";
+                fileName = "adb.exe";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                folderName = "linux";
+                fileName = "adb";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                folderName = "darwin";
+                fileName = "adb";
+            }
+            string fullLocalADBPath = Path.Combine(AppContext.BaseDirectory, "adb", folderName, fileName);
+            ProcessStartInfo info = new ProcessStartInfo(File.Exists(fullLocalADBPath) ? fullLocalADBPath : fileName, Command)
+            {
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+            process.StartInfo = info;
+            process.Start();
+            return (process.StandardOutput.ReadToEnd() + "\r\n" + process.StandardError.ReadToEnd()).Trim();
+        }
+    }
+}
+
