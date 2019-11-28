92dd1d707e126e1cf37abae60782bc93fce8c114
Devil7DK <dineshthangavel47@gmail.com>
2019-11-28 12:33:46 +0530

    Implement InverseBoolConverter

--- AndroidSepolicyHelper/Utils/Converters.cs
+++ AndroidSepolicyHelper/Utils/Converters.cs
@@ -30,4 +30,20 @@ namespace AndroidSepolicyHelper.Utils
         #endregion
 
     }
+
+    public class InverseBoolConverter : IValueConverter
+    {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+        #endregion
+
+    }
 }
