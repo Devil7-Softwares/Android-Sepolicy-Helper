08fe211d97bb1b8c5d1d421de5e9731248daf774
Devil7DK <dineshthangavel47@gmail.com>
2019-11-28 11:04:01 +0530

    Add Sepolicy Info Model

---
+++ AndroidSepolicyHelper/Models/SepolicyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace AndroidSepolicyHelper.Models
+{
+    public class SepolicyInfo
+    {
+        #region Constructor
+        public SepolicyInfo(string Action, string Source, string Target, string TargetClass)
+        {
+            this.Action = Action;
+            this.Source = Source;
+            this.Target = Target;
+            this.TargetClass = TargetClass;
+        }
+        #endregion
+
+        #region Properties
+        [Browsable(false)]
+        public string Action { get; set; }
+
+        [Browsable(false)]
+        public string Reference { get; set; }
+
+        public string Sepolicy { get; set; }
+
+        [Browsable(false)]
+        public string Source { get; set; }
+
+        [Browsable(false)]
+        public string Target { get; set; }
+
+        [Browsable(false)]
+        public string TargetClass { get; set; }
+        #endregion
+    }
+}
+
