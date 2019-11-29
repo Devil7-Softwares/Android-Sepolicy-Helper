52e472b1ca310744849b5e208899d2b2a5dd7d03
Devil7DK <dineshthangavel47@gmail.com>
2019-11-29 13:08:24 +0530

    Implement MesssageBox Helper

---
+++ AndroidSepolicyHelper/Utils/MessageBoxHelper.cs
@@ -0,0 +1,56 @@
+﻿using Avalonia.Controls;
+using MessageBox.Avalonia;
+using MessageBox.Avalonia.DTO;
+using MessageBox.Avalonia.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidSepolicyHelper.Utils
+{
+    public static class MessageBoxHelper
+    {
+        public static async Task Info(string message, string title, Window parent = null)
+        {
+            await Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(async () =>
+            {
+                MessageBoxWindow messageBox = new MessageBoxWindow(new MessageBoxParams()
+                {
+                    Button = ButtonEnum.Ok,
+                    CanResize = false,
+                    ContentMessage = message,
+                    ContentTitle = title,
+                    Icon = Icon.Success,
+                    ShowInCenter = true,
+                    WindowIcon = null
+                });
+                if (parent == null)
+                    await messageBox.Show();
+                else
+                    await messageBox.ShowDialog(parent);
+            });
+        }
+
+        public static async Task Error(string message, string title, Window parent = null)
+        {
+            await Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(async () =>
+            {
+                MessageBoxWindow messageBox = new MessageBoxWindow(new MessageBoxParams()
+                {
+                    Button = ButtonEnum.Ok,
+                    CanResize = false,
+                    ContentMessage = message,
+                    ContentTitle = title,
+                    Icon = Icon.Error,
+                    ShowInCenter = true,
+                    WindowIcon = null
+                });
+                if (parent == null)
+                    await messageBox.Show();
+                else
+                    await messageBox.ShowDialog(parent);
+            });
+        }
+    }
+}
